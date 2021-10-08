7408ef85f697993357710c74cce7db7350e27a37
Jose Nieto <inge1neuro@gmail.com>
2021-10-08 11:45:37 -0500

    Agregando endpoint para refrescar token de sesión.

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
 
     Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
         Route::post('logout', [AuthController::class, 'logout'])->name('logout');
         Route::get('me', [AuthController::class, 'me'])->name('me');
     });
